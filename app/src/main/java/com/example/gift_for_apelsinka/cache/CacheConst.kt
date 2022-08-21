0174791ebc29f5552eef352227f21da6d0bacf50
Rylexium <aleksei.azizov@mail.ru>
2022-08-21 15:18:10 +0400

    remove some photos

---
+++ app/src/main/java/com/example/gift_for_apelsinka/cache/CacheConst.kt
@@ -0,0 +1,51 @@
+package com.example.gift_for_apelsinka.cache
+
+import com.example.gift_for_apelsinka.activity.main.model.Statement
+import com.example.gift_for_apelsinka.activity.photo.model.FieldPhoto
+import com.example.gift_for_apelsinka.cache.model.Picture
+
+val statementList : List<Statement>
+    get() {
+        TODO()
+    }
+val mainPictures : List<Picture>
+    get() {
+        TODO()
+    }
+val aboutApelsinka : String
+    get() {
+        TODO()
+    }
+val logoPictures : List<Picture>
+    get() {
+        TODO()
+    }
+val aboutOscar : String
+    get() {
+        TODO()
+    }
+val oscarPicture : List<Picture>
+    get() {
+        TODO()
+    }
+val aboutLera : String
+    get() {
+        TODO()
+    }
+val leraPicture : List<Picture>
+    get() {
+        TODO()
+    }
+val aboutLexa : String
+    get() {
+        TODO()
+    }
+val lexaPicture : List<Picture>
+    get() {
+        TODO()
+    }
+
+val pictureList : List<FieldPhoto>
+    get() {
+        TODO()
+    }
---
+++ app/src/main/java/com/example/gift_for_apelsinka/cache/model/Picture.kt
@@ -0,0 +1,4 @@
+package com.example.gift_for_apelsinka.cache.model
+
+data class Picture(val id : Int,
+                   val drawable : Any)
