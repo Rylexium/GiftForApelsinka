911b14628eb07c07eca21e4c8e7f15a80f2e7ba1
Rylexium <aleksei.azizov@mail.ru>
2022-08-27 18:17:36 +0400

    work with notifications

--- app/src/main/java/com/example/gift_for_apelsinka/retrofit/service/NotificationsServiceApi.kt
+++ app/src/main/java/com/example/gift_for_apelsinka/retrofit/service/NotificationsServiceApi.kt
@@ -1,11 +1,8 @@
 package com.example.gift_for_apelsinka.retrofit.service
 
-import com.example.gift_for_apelsinka.retrofit.requestmodel.NotificationID
 import com.example.gift_for_apelsinka.retrofit.requestmodel.response.NotificationList
 import retrofit2.Call
-import retrofit2.http.Body
 import retrofit2.http.GET
-import retrofit2.http.POST
 import retrofit2.http.Query
 
 interface NotificationsServiceApi {
